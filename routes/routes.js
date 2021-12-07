15f0fe59c7e80c4c4980fc94c387bfed60ea80d8
Kaleb Washington <kdwashington302@gmail.com>
2021-12-06 19:04:40 -0700

    log in working

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 
 const url = `mongodb+srv://group:pro150@userinformation.xrasy.mongodb.net/UserInformation?retryWrites=true&w=majority`;
 
+
+
 const client =  new MongoClient(url);
 
 const dbName = 'UserInformation';
@@ -34,7 +36,8 @@ exports.signUpAction = async (req, res) => {
         username: req.body.username,
         password: hash,
         currency:1000,
-        email: req.body.email
+        email: req.body.email,
+        nickName: req.body.username
     }
     const insertResult = await userCollection.insertOne(account);
     client.close();
@@ -49,7 +52,6 @@ exports.logInAction = async (req, res) => {
     await client.connect();
     console.log(req.body.password)
     const userResults = userCollection.find({username: req.body.username})
-    client.close();
     if(bcrypt.hashSync(req.body.password, userResults.password)){
         req.session.user = {
             isAuthenticated: true,
@@ -62,6 +64,7 @@ exports.logInAction = async (req, res) => {
     }else{
         res.redirect("login")
     }
+    client.close();
 }
 
 exports.dashboard = (req, res) => {
